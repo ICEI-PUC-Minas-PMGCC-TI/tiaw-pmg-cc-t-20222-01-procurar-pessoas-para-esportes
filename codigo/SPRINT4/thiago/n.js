93541e45b007759fb17db6da8ae81803e1b86646
CaioFD <105741319+CaioFD@users.noreply.github.com>
2022-12-04 17:52:28 -0300

    Add files via upload

---
+++ codigo/SPRINT4/thiago/n.js
@@ -0,0 +1,22 @@
+[{
+
+  "mensagens": "notificações",
+  "objetivo": {
+    "tipo": "solicitação de amizade",
+    "horario": "22 de outubro de 2022",
+    "opções": ["aceitar", "recusar"]
+  },
+
+  "objetivo": {
+    "tipo": "recebimento de mensagem",
+    "horario": "22 de outubro de 2022",
+    "opção": "responder",
+  },
+
+  "objetivo": {
+    "tipo": "convite para grupos",
+    "horario": "22 de outubro de 2022",
+    "opções": ["aceitar", "recusar"]
+  }
+
+}]
